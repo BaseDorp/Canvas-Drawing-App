aba37960d2539799964a250fd92d63898c3e20f9
Sam Hirsch <samhirsch.dev@gmail.com>
2022-02-07 01:18:56 -0600

    can undo  squares and circles

--- index.js
+++ index.js
@@ -3,27 +3,13 @@ const context = canvas.getContext("2d");
 
 let outputField = document.getElementById("outputText");
 let sizeSlider = document.getElementById("sizeSlider");
+let colorInput = document.getElementById("colorInput");
 
-
-// Gets the values from the RBG sliders and creates a preview color
-let redSlider = document.getElementById("redSlider");
-let greenSlider = document.getElementById("greenSlider");
-let blueSlider = document.getElementById("blueSlider");
-redSlider.addEventListener("input", updateColorPreview)
-greenSlider.addEventListener("input", updateColorPreview)
-blueSlider.addEventListener("input", updateColorPreview)
-
-// sets div to the rbg values of the sliders
-let colorPreview = document.getElementById("color-preview");
-colorPreview.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
-
-function updateColorPreview()
-{
-    colorPreview.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
-
-    context.beginPath();
-}
+// Resets the line being drawn whenever the color or brush size is changed
 sizeSlider.addEventListener("input", ()=>{
+    context.beginPath();
+})
+colorInput.addEventListener("input", ()=>{
     context.beginPath();
 })
 
@@ -34,9 +20,18 @@ function ToolChange(toolName){
     toolSelected = toolName;
 }
 
-let painting = false;
+// path - a Path2D object // style - stroke style value
+function Shape(path, style){
+    this.path = path;
+    this.style = style;
+};
 
+// history of paths drawn to the canvas
+let history = [];
+
+let newPath = new Path2D();
 context.beginPath();
+let painting = false;
 canvas.addEventListener('mousedown', (e) => {
     // Get mouse position on the canvas relative to the screen space
     let mouseX = e.clientX - canvas.getBoundingClientRect().left;
@@ -44,59 +39,62 @@ canvas.addEventListener('mousedown', (e) => {
     let startX = mouseX;
     let startY = mouseY;
 
-    // Get color from rgb sliders
-    context.fillStyle = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+    // set color
+    // context.fillStyle = colorInput.value;
+    // context.strokeStyle = colorInput.value;
 
     switch (toolSelected)
     {
         case "Paint":
             painting = true;
-            draw(e);
+            // draw(e);
             break;
-
         case "Fill":
 
             break;
-
         case "Line":
-            context.strokeStyle = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
             context.lineWidth = sizeSlider.value;
             context.lineJoin = "round";
             context.lineCap = "round";
+            context.strokeStyle = colorInput.value;
+
+            // let newLine = new Path2D();
+            // newLine.lineTo(mouseX, mouseY);
+            // context.stroke(newLine);
+            // history.push(new Shape(newLine, colorInput.value));
+            
             
             context.lineTo(mouseX, mouseY);
             context.stroke();
-            break;
 
+            break;
         case "Circle":
-            context.lineWidth = 0;
-            context.beginPath();
-            context.arc(mouseX, mouseY, sizeSlider.value, 0, 360);
-            context.fill();
+            let newCircle = new Path2D();
+            newCircle.arc(mouseX, mouseY, sizeSlider.value, 0, 360);
+            context.strokeStyle = colorInput.value;
+            context.stroke(newCircle);
+            history.push(new Shape(newCircle, colorInput.value));
             break;
-
         case "Square":
-            context.fillRect(mouseX - sizeSlider.value/2, mouseY - sizeSlider.value/2, sizeSlider.value, sizeSlider.value);
+            let newSqaure = new Path2D();
+            newSqaure.rect(mouseX - sizeSlider.value/2, mouseY - sizeSlider.value/2, sizeSlider.value, sizeSlider.value)
+            context.strokeStyle = colorInput.value;
+            context.stroke(newSqaure);
+            history.push(new Shape(newSqaure, colorInput.value));
             break;
     }
 });
 
 // 
 canvas.addEventListener("mouseup", () => {
-    if (toolSelected == "Paint"){
-    context.beginPath();
     painting = false;
-    }
 });
-canvas.addEventListener("mousemove", draw);
-
-function draw(e)
-{
+canvas.addEventListener("mousemove", (e)=>{
     if (!painting) return;
 
     // paint styling
     context.beginPath();
-    context.strokeStyle = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`;
+    context.strokeStyle = colorInput.value;
     context.lineWidth = sizeSlider.value;
     context.lineCap = "round";
 
@@ -104,10 +102,40 @@ function draw(e)
     context.stroke();
     context.beginPath();
     context.moveTo(e.clientX - canvas.getBoundingClientRect().left, e.clientY - canvas.getBoundingClientRect().top);
+});
+
+// function draw(e)
+// {
+    
+// }
+
+// Removes the last object drawn and redraws the canvas
+function Undo(){
+    if (history.length == 0) return;
+    history.pop();
+    UpdateCanvas();
 }
 
-//
+function Redo(){
+
+}
+
+// Erases the canvas and redraws the canvas from the array of drawn objects
+function UpdateCanvas(){
+    ClearCanvas();
+    for (let i = 0; i < history.length; i++){
+        context.strokeStyle = history[i].style;
+        context.stroke(history[i].path);
+    }
+}
+
+// Wipes the canvas clean
 function ClearCanvas(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     drawingLine = false;
+}
+
+// Creates a new canvas layer
+function NewLayer(){
+
 }
