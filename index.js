ec9c7e6f730ad2030b2530c837c362d759da3808
Sam Hirsch <samhirsch.dev@gmail.com>
2022-02-17 15:32:14 -0600

    can undo line segments now

--- index.js
+++ index.js
@@ -6,38 +6,31 @@ let sizeSlider = document.getElementById("sizeSlider");
 let colorInput = document.getElementById("colorInput");
 
 // Resets the line being drawn whenever the color or brush size is changed
-sizeSlider.addEventListener("input", ()=>{
-    context.beginPath();
-    newLine = new Path2D();
-})
-colorInput.addEventListener("input", ()=>{
-    context.beginPath();
-    newLine = new Path2D();
-})
+sizeSlider.addEventListener("input", () => {
+  context.beginPath();
+});
+colorInput.addEventListener("input", () => {
+  context.beginPath();
+});
 
 // Changes which tool the user has selected
 let toolSelected = "Paint";
-function ToolChange(toolName){
-    outputField.innerText = toolName + " Tool Selected";
-    toolSelected = toolName;
-    newLine = new Path2D();
+function ToolChange(toolName) {
+  outputField.innerText = toolName + " Tool Selected";
+  toolSelected = toolName;
+  context.beginPath();
 }
 
 // path - a Path2D object // style - stroke style value // type - type of shape (Square or Circle) //
-function Shape(path, style, type){
-    this.path = path;
-    this.style = style;
-    this.type = type;
-};
-function Drawing(points, style, size, type){
-    this.points = points;
-    this.style = style;
-    this.size = size;
-    this.type = type;
+function Shape(path, style, type, size) {
+  this.path = path;
+  this.style = style;
+  this.type = type;
+  this.size = size;
 }
-function Point(X, Y){
-    this.X = X;
-    this.Y = Y;
+function Point(X, Y) {
+  this.X = X;
+  this.Y = Y;
 }
 
 // history of paths drawn to the canvas
@@ -45,139 +38,123 @@ let history = [];
 let points = [];
 
 let newPath = new Path2D();
-let newLine = new Path2D();
-context.beginPath();
 let painting = false;
-canvas.addEventListener('mousedown', (e) => {
-    // Get mouse position on the canvas relative to the screen space
-    let mouseX = e.clientX - canvas.getBoundingClientRect().left;
-    let mouseY = e.clientY - canvas.getBoundingClientRect().top;
+context.lineJoin = "round";
+context.lineCap = "round";
 
-    // set color
-    // context.fillStyle = colorInput.value;
-    // context.strokeStyle = colorInput.value;
+canvas.addEventListener("mousedown", (e) => {
+  // Get mouse position on the canvas relative to the screen space
+  let mouseX = e.clientX - canvas.getBoundingClientRect().left;
+  let mouseY = e.clientY - canvas.getBoundingClientRect().top;
 
-    switch (toolSelected)
-    {
-        case "Paint":
-            painting = true;
-            draw(e);
-            break;
-        case "Fill":
+  context.strokeStyle = colorInput.value;
 
-            break;
-        case "Line":
-            context.lineWidth = sizeSlider.value;
-            context.lineJoin = "round";
-            context.lineCap = "round";
-            context.strokeStyle = colorInput.value;
+  switch (toolSelected) {
+    case "Paint":
+      painting = true;
+      draw(e);
+      break;
+    case "Line":
+      context.lineWidth = sizeSlider.value;
+      context.strokeStyle = colorInput.value;
 
-            newLine.lineWidth = sizeSlider.value;
-            newLine.lineTo(mouseX, mouseY);
-            context.stroke(newLine);
-            history.push(new Shape(newLine, colorInput.value));
+      context.lineTo(mouseX, mouseY);
+      context.stroke();
+      history.push(new Shape(new Point(mouseX, mouseY), colorInput.value, "Line", sizeSlider.value));
+      break;
+    case "Circle":
+      let newCircle = new Path2D();
+      context.lineWidth = 1;
+      newCircle.arc(mouseX, mouseY, sizeSlider.value, 0, 360);
 
-            break;
-        case "Circle":
-            let newCircle = new Path2D();
-            newCircle.arc(mouseX, mouseY, sizeSlider.value, 0, 360);
-            context.strokeStyle = colorInput.value;
-            context.stroke(newCircle);
-            history.push(new Shape(newCircle, colorInput.value, "Circle"));
-            break;
-        case "Square":
-            let newSqaure = new Path2D();
-            newSqaure.rect(mouseX - sizeSlider.value/2, mouseY - sizeSlider.value/2, sizeSlider.value, sizeSlider.value)
-            context.strokeStyle = colorInput.value;
-            context.stroke(newSqaure);
-            history.push(new Shape(newSqaure, colorInput.value, "Square"));
-            break;
-    }
+      context.stroke(newCircle);
+      history.push(new Shape(newCircle, colorInput.value, "Circle"));
+      break;
+    case "Square":
+      let newSqaure = new Path2D();
+      context.lineWidth = 1;
+      newSqaure.rect(
+        mouseX - sizeSlider.value / 2,
+        mouseY - sizeSlider.value / 2,
+        sizeSlider.value,
+        sizeSlider.value
+      );
+      context.stroke(newSqaure);
+      history.push(new Shape(newSqaure, colorInput.value, "Square"));
+      break;
+  }
 });
 
-// 
+//
 canvas.addEventListener("mouseup", () => {
-    if (!painting) return;
-    painting = false;
-    history.push(new Drawing(points, colorInput.value, sizeSlider.value, "Paint"));
-    points = [];
-    newPath = new Path2D();
+  if (!painting) return;
+  painting = false;
+  history.push(new Shape(points, colorInput.value, "Paint", sizeSlider.value));
+  points = [];
+  newPath = new Path2D();
 });
 canvas.addEventListener("mousemove", draw);
 
-function draw(e)
-{
-    if (!painting) return;
+function draw(e) {
+  if (!painting) return;
 
-    // Get mouse position on the canvas relative to the screen space
-    let mouseX = e.clientX - canvas.getBoundingClientRect().left;
-    let mouseY = e.clientY - canvas.getBoundingClientRect().top;
+  // Get mouse position on the canvas relative to the screen space
+  let mouseX = e.clientX - canvas.getBoundingClientRect().left;
+  let mouseY = e.clientY - canvas.getBoundingClientRect().top;
 
-    // paint styling
-    newPath = new Path2D();
-    context.strokeStyle = colorInput.value;
-    context.lineWidth = sizeSlider.value;
-    context.lineCap = "round";
+  // paint styling
+  newPath = new Path2D();
+  context.strokeStyle = colorInput.value;
+  context.lineWidth = sizeSlider.value;
+  context.lineCap = "round";
 
-    // context.beginPath();
-    // context.lineTo(mouseX, mouseY);
-    // context.stroke();
-    // context.beginPath();
-    // context.moveTo(mouseX, mouseY);
-    newPath.lineTo(mouseX, mouseY);
-    points.push(new Point(mouseX, mouseY));
-    context.stroke(newPath);
+  newPath.lineTo(mouseX, mouseY);
+  points.push(new Point(mouseX, mouseY));
+  context.stroke(newPath);
 }
 
 // Removes the last object drawn and redraws the canvas
-function Undo(){
-    if (history.length == 0) return;
-    // newLine = new Path2D();
-    ClearCanvas();
-    history.pop();
-    UpdateCanvas();
-    
-    console.log(history);
-}
-
-function Redo(){
-
+function Undo() {
+  if (history.length == 0) return;
+  ClearCanvas();
+  history.pop();
+  UpdateCanvas();
 }
 
 // Erases the canvas and redraws the canvas from the array of drawn objects
-function UpdateCanvas(){
-    ClearCanvas();
-    for (let i = 0; i < history.length; i++){
-        context.lineCap = "round";
-        context.strokeStyle = history[i].style;
-        context.lineWidth = history[i].size;
+function UpdateCanvas() {
+  ClearCanvas();
+  for (let i = 0; i < history.length; i++) {
+    context.lineCap = "round";
+    context.strokeStyle = history[i].style;
+    context.lineWidth = history[i].size;
 
-        if (history[i].type == "Square" || history[i].type == "Circle"){
-            context.lineWidth = 1;
-            context.stroke(history[i].path);
-        }
-
-        // TODO WORK WITH LINES
-        
-
-        if (history[i].type == "Paint"){
-            for (let j = 0; j < history[i].points.length; j++){
-                context.beginPath();
-                context.lineTo(history[i].points[j].X, history[i].points[j].Y);
-                context.stroke();
-            }
-        }
-        
+    if (history[i].type == "Square" || history[i].type == "Circle") {
+      context.lineWidth = 1;
+      context.stroke(history[i].path);
+    } 
+    else if (history[i].type == "Paint") {
+      for (let j = 0; j < history[i].path.length; j++) {
+        context.beginPath();
+        context.lineTo(history[i].path[j].X, history[i].path[j].Y);
+        context.stroke();
+      }
     }
+    else if (history[i].type == "Line"){
+        context.lineTo(history[i].path.X, history[i].path.Y);
+        context.stroke();
+    }
+  }
 }
 
 // Wipes the canvas clean
-function ClearCanvas(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawingLine = false;
+function ClearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawingLine = false;
+  context.beginPath();
 }
 
 // Creates a new canvas layer
-function NewLayer(){
-
+function NewLayer() {
+    
 }
